85d5fa709cb236e2e61b34d89512d3ced66d9011
Сергей <rokyen1989@mail.ru>
2016-03-14 22:51:36 +0400

    //

--- src/ru/grishin/Chdir.java
+++ src/ru/grishin/Chdir.java
@@ -4,9 +4,6 @@ import java.io.File;
 
 public class Chdir extends BasicCommand{
 
-    public static String CHDIR = "";
-    public static boolean BOOLCHDIR = false;
-
     protected Chdir(){
         super("chdir");
     }
@@ -18,26 +15,14 @@ public class Chdir extends BasicCommand{
 
     @Override
     public void execute(String root,String[] args) {
-        if (args[0].equals("..")){
-            String[] url = Command.COMMLIST.toArray(new String[Command.COMMLIST.size()]);
-            if (url.length > 1){
-                Command.COMMLIST.remove(url.length -1);
-                CHDIR = url[url.length - 2];
-                BOOLCHDIR = true;
-            } else {
-                CHDIR = url[0];
-                Command.COMMLIST.clear();
-                Command.COMMLIST.add(url[0]);
-                BOOLCHDIR = true;
-            }
+        if (args[0].equals("..") & root.length() > 3){
+            Command.COMM = new File(root).getParent();
         } else {
-            File myFile = new File(args[0]);
-            File[] files = myFile.listFiles();
-            if (files != null) {
-                CHDIR = args[0];
-                Command.COMMLIST.clear();
-                Command.COMMLIST.add(args[0]);
-                BOOLCHDIR = true;
+            if (!args[0].equals("..")) {
+                java.io.File file = new java.io.File(args[0]);
+                if (file.exists()) {
+                    Command.COMM = args[0];
+                }
             }
         }
     }
--- src/ru/grishin/Command.java
+++ src/ru/grishin/Command.java
@@ -1,41 +1,33 @@
 package ru.grishin;
 
 import java.io.File;
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 import java.util.Scanner;
 
 public class Command {
 
-    public static List <String> COMMLIST = new ArrayList<>();
+    public static String COMM;
 
     public static void main(String[] args1){
-        String comm = "";
+        COMM = new File("").getAbsolutePath();
         CommandList cl = new CommandList();
         while (true) {
-            if (Chdir.BOOLCHDIR){
-                comm = Chdir.CHDIR;
-                Chdir.BOOLCHDIR = false;
-            }
-            System.out.print(comm +">");
+            System.out.print(COMM +">");
             Scanner scan = new Scanner(System.in);
             String line = scan.nextLine();
             if (line.endsWith("\\")){
-                comm += line;
-                File file = new File(comm);
+                COMM += line;
+                File file = new File(COMM);
                 File[] files = file.listFiles();
                 if (files == null) {
-                    comm = comm.substring(0, comm.length() - line.length());
-                } else {
-                    COMMLIST.add(comm);
+                    COMM = COMM.substring(0, COMM.length() - line.length());
                 }
             }
             String[] args = line.split(" ");
             BasicCommand command = cl.getCommandByName(args[0]);
             if (command != null){
                 String[] s = Arrays.copyOfRange(args, 1, args.length);
-                command.execute(comm ,s);
+                command.execute(COMM ,s);
             }
         }
     }
